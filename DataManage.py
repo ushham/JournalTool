f6e9179018a052ceee7ac2e32f2d40b7acf6fd6f
ushham <ush.hamilton@gmail.com>
2021-03-12 23:42:11 +0000

    Update to database manager

--- DataManage.py
+++ DataManage.py
@@ -1,23 +1,25 @@
 import json
 import os
 import datetime as dt
+import control as ct
 
 class DataManage:
-    folder = '/Users/ushhamilton/Documents/02 Journals/Journals/'
-    filetype = '.md'
+    title = '# '
     status = 'Status: '
     tags = 'Tags: '
+    mood = 'Mood: '
+    feel = 'Score: '
 
     def __init__(self, day):
         self.date = day
 
     def listall(self):
         #List all files in subfolders
-        locs = os.walk(self.folder)
+        locs = os.walk(ct.folder)
         f_loc = []
         for x in locs:
             for f in os.listdir(x[0]):
-                if f.endswith(self.filetype):
+                if f.endswith(ct.ext):
                     add = x[0] + '/' + f
                     add = add.replace('//', '/')
                     f_loc.append(add)
@@ -29,27 +31,38 @@ class DataManage:
         data_date = dt.datetime.strptime(str(20) + data_date, '%Y%m%d')
 
         lines = data.readlines()
+        count = 0
         for line in lines:
+            count += 1
             #Extract journal status
-            if self.status in line:
+            if (self.title in line) & (count == 1):
+                tit = line.replace(self.title, '').strip('\n')
+            elif self.status in line:
                 stat = line.replace(self.status, '').strip('\n')
             
             #Extract journal tags
             elif self.tags in line:
                 tag = line.replace(self.tags, '').strip('\n')
 
-        return [data_date, stat, tag]
+            elif self.mood in line:
+                mod = line.replace(self.mood, '').strip('\n')
+            
+            elif self.feel in line:
+                score = line.replace(self.feel, '').strip('\n')
+
+        return [data_date, tit, stat.split(', '), tag.split(', '), mod.split(', '), int(score)]
 
     def make_base(self):
         #open all files and read
         #title, date, status, tags
         files = self.listall()
-        print(files)
-        for f in files[1:]:
-            print(f)
-            data = open(f)
-            line = self.parse_data(f, data)
-        return line
+        hold = []
+        for f in files:
+            if ct.temp not in f:
+                data = open(f)
+                line = self.parse_data(f, data)
+                hold.append(line)
+        return hold
 
 x = DataManage(1)
 y = x.make_base()
--- FileManage.py
+++ FileManage.py
@@ -2,24 +2,61 @@ import json
 import os
 import datetime as dt
 import subprocess
+import shutil
+import control as ct
 
 class FileManage:
 
-    folder = '/Users/ushhamilton/Documents/02 Journals/Journals/'
+    ext = '.md'
+
     def __init__(self, day):
         self.date = day
 
-    def openfile(self, sub, file):
-        file_name = self.folder + sub + file
+    def openfile(self, path):
+        file_name = ct.folder + path
         subprocess.run(['open', file_name], check=True)
         return 0
 
-    def readfile(self, sub, file):
-        file_name = self.folder + sub + file
+    def readfile(self, path):
+        file_name = ct.folder + path
         data = open(file_name)
         return data
 
+    def makefolder(self, d):
+        #Given date, makes subfolders for file
+        year = d.strftime('%Y')
+        month_num = d.strftime('%m')
+        month_name = d.strftime('%b')
+        #check year forder exists
+        yr_fold = ct.folder + year
+        if not os.path.exists(yr_fold):
+            os.makedirs(yr_fold)
+        
+        #Check if month folder exists
+        mth_fold = yr_fold + '/' + month_num + ' ' + month_name
+        if not os.path.exists(mth_fold):
+            os.makedirs(mth_fold)
+        
+        return 0
+
+    def copytemplate(self, date):
+        #Given date, copys template to location and saves with name and opens
+        year, month, day, m_name = date.strftime('%Y'), date.strftime('%m'), date.strftime('%d'), date.strftime('%b')
+        name = year[2:] + month + day
+        title = input('Journal name: ')
+        name = name + ' ' + title + self.ext
+        
+        path = ct.folder + year + '/' + month + ' ' + m_name 
+        if not os.path.exists(path):
+            self.makefolder(date)
+
+        print(path)
+        shutil.copy(ct.folder + ct.temp, path)
+        os.rename(path + '/' + ct.temp, path + '/' + name)
+        self.openfile(year + '/' + month + ' ' + m_name + '/' + name)
+        return 0
+
 
 x = FileManage(1)
-y = x.readfile('2021/03 Mar/', '210311 Testing the system.md')
-print(y.read())
+d = dt.datetime.now()
+x.copytemplate(d)
