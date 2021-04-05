767750a65cf27435b79e328526111e5796a0b7d3
ushham <ush.hamilton@gmail.com>
2021-04-05 22:07:00 +0100

    Adding status filter to dropdown

--- FileMaker.py
+++ FileMaker.py
@@ -43,22 +43,22 @@ class FileMake:
         
         return 0
 
-    def makeyear(self, my_date=dt.date.today()):
+    def makeyear(self, stat, my_date=dt.date.today()):
         def conv_date(s):
             return dt.datetime.strptime(s, '%Y-%m-%d').date()
 
         data_base = self.datar.open_base()
         my_month, my_day = my_date.month, my_date.day
 
-
         dates = [(conv_date(d['Date']), d['File'], d['Title']) for d in data_base if 
         (conv_date(d['Date']).month == my_month) and 
-        (conv_date(d['Date']).day == my_day)]
+        (conv_date(d['Date']).day == my_day) and
+        ((stat in d['Status']) or stat == 'All')]
 
         self.combinefile('My Year', dates, rev=True)
         return 0
 
-    def makeperiod(self, period, name='Custom', my_date=dt.date.today()):
+    def makeperiod(self, period, stat, name='Custom', my_date=dt.date.today()):
         def conv_date(s):
             return dt.datetime.strptime(s, '%Y-%m-%d').date()
 
@@ -68,7 +68,8 @@ class FileMake:
 
         dates = [(conv_date(d['Date']), d['File'], d['Title']) for d in data_base if 
         (conv_date(d['Date']) > my_date - dt.timedelta(days = period)) and
-        (conv_date(d['Date']) <= my_date)]
+        (conv_date(d['Date']) <= my_date) and
+        ((stat in d['Status']) or stat == 'All')]
 
 
         self.combinefile('My ' + name, dates, rev=True)
