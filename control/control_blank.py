7a4aa92a878e6b9fe941325a199243fec24682ac
ushham <ush.hamilton@gmail.com>
2023-06-04 12:38:44 +0100

    gitignore update

--- control/control_blank.py
+++ control/control_blank.py
@@ -1,9 +1,9 @@
 import datetime as dt
 
-folder = '/Users/ushhamilton/Documents/02_Journals/Journals/'
-location_folder = '/Users/ushhamilton/Documents/02 Journals/Locations/'
+folder = ''
+location_folder = ''
 
-time_cap_folder = '/Users/ushhamilton/Documents/02_Journals/TimeCapsules'
+time_cap_folder = ''
 
 temp = 'Template.md'
 data_b = 'JournalDatabase.json'
@@ -17,7 +17,7 @@ temp_folder = 'Temps/'
 
 
 #first date of useable data
-first_date = dt.date(2021, 3, 1)
+first_date = dt.date(2023, 6, 4)
 
 #Plotting constants
 min_plt, max_plt = 0, 10
@@ -39,7 +39,7 @@ life_expectancy = 80   #<- Last year that is shown in the chart.
 standard_weeks_year = 52
 weeks_per_year = 52.17857143  #365.25 / 7
 days_per_week = 7
-dob = '24/05/1994'      #<- Date of birth to be edited.
+dob = ''      #<- Date of birth to be edited.
 
 event = 'Life Event'    #<- The title in the database given to life events
 
